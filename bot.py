db89424c0b17063a67c9c899a1bae53d9cc59ae6
maashkaa <mari_aleksandrovna_1997@mail.ru>
2026-02-28 20:55:09 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ConversationHandler, ContextTypes, CallbackQueryHandler, filters
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("8653328794:AAGrsAs6OzF4hIZusXB8CENoyMjcBXpaguM")
 PASSWORD = "1234"
 ADMIN_ID = 891530001
 
